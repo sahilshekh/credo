7e051ed772fd12bd69f4dd3fe1bb19e57b6a1ee8
Sahil Shekh <95908470+sahilshekh@users.noreply.github.com>
2022-04-03 17:07:25 +0530

    update

--- static/jsscripts/footer.js
+++
@@ -1,4 +0,0 @@
-import footer from "./footerlogic.js"
-
-let lower=document.getElementById("bigcontainer")
-lower.innerHTML=footer()
--- static/jsscripts/nav.js
+++
@@ -1,3 +0,0 @@
-import navBar from "./navbar.js"
-let nav = document.getElementById("navbar")
-nav.innerHTML = navBar()
